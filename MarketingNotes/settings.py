75499c1140193eb64dfa945a1c5e373ed3c046d7
Cyberspyde <52118091+cyberspyde@users.noreply.github.com>
2023-05-14 18:29:10 +0900

    'settings'

--- MarketingNotes/settings.py
+++ MarketingNotes/settings.py
@@ -40,7 +40,6 @@ DJANGORESIZED_DEFAULT_NORMALIZE_ROTATION = True
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
-#    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -114,8 +113,6 @@ MEDIA_ROOT =  '/home/fraydeyu/public_html/fraydey.uz/media'
 MEDIA_URL = '/fraydey.uz/media/'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
-
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
